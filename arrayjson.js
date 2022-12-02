77d25d4546f7da4ece1c377157558a4f8f3c5377
Learner Dev Anand <72151612+onlyanand10@users.noreply.github.com>
2022-12-02 15:38:52 +0530

    cowsays

---
+++ arrayjson.js
@@ -0,0 +1,11 @@
+//array
+const a = ["anand ", 10, true, undefined]; // can contain anything
+
+// json  : key value pair
+const jsona = {
+  name: "ANand",
+  surname: "Suryawanshi",
+  foodLinked: ["sweet", "pan"],
+};
+console.log(a);
+console.log(jsona.name);
